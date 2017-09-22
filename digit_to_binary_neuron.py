d6c1649b4c829581ff829221350abd8de6c74905
bgravy <bgraefe@gmail.com>
2017-09-22 16:28:45 -0500

    Finshed sigmoid output function

--- digit_to_binary_neuron.py
+++ digit_to_binary_neuron.py
@@ -1,1 +1,13 @@
 #neural networks and deep learning example - map 0-9 outputs to 4 binary output neurons
+import math
+
+#compute the latest output of a sigmoid neuron
+def sigmoid_output(inputs_list,weights_list,bias)
+  weighted_sum=0
+  for input_index in range(0,length(input_weights)):
+    weighted_sum = weighted_sum + inputs_list[input_index] * weights_list[input_index]
+  weighted_sum = weighted_sum + bias
+  exponent = exp(-weighted_sum)
+  denominator = 1 + exponent
+  output_return = 1/denominator
+  return output_return
